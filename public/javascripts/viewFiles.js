27cb91e8771c53fbbb01c51c546a9a46bcc83f10
Hugh <hughriegel@gmail.com>
2016-09-30 12:15:27 -0400

    view files now has proper styling

--- public/javascripts/viewFiles.js
+++ public/javascripts/viewFiles.js
@@ -15,8 +15,16 @@
         for(var i = 0; i < files.length; i++) {
             var file = files[i];
             $('#uploadedFileList').append(
-                $("<div></div>").text(file),
-                $("<div></div>").text(file)
+                $('<div class="row uploaded-file-row"></div>').append(
+                    $('<div id="fileList_title" class="col-md-6"></div>').text(file),
+                    $('<div id="fileList_extension" class="col-md-3">ID</div>'),
+                    $('<div class="col-md-2"></div>').append(
+                        $('<button id="fileList_edit" class="btn btn-lg sm-button edit-btn sm-edit">Edit</button>')
+                    ),
+                    $('<div class="col-md-1"></div>').append(
+                        $('<button id="fileList_delete" class="btn btn-lg sm-button edit-btn sm-delete">X</button>')
+                    )
+                )
             );
         }
     } resetFileList();
