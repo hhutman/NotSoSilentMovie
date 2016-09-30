f8cab6b76644779a0a07448a159d3162d226a935
Hugh <hughriegel@gmail.com>
2016-09-30 17:55:35 -0400

    viewFile list now correctly connects to each file target

--- public/javascripts/viewFiles.js
+++ public/javascripts/viewFiles.js
@@ -17,19 +17,25 @@
             $('#uploadedFileList').append(
                 $('<div class="row uploaded-file-row"></div>').append(
                     $('<div class="col-xs-1"></div>'),
-                    $('<div id="fileList_title" class="col-xs-5"></div>').text(file.name),
+                    $('<div class="col-xs-5"></div>').text(file.name)
+                        .attr('id', 'fileList_title' + i),
                     $('<div id="fileList_extension" class="col-xs-2"></div>').text(file.fileType),
                     $('<div class="col-xs-2"></div>').append(
                         $('<button id="fileList_edit" class="btn btn-lg sm-button edit-btn sm-edit">Edit</button>')
                     ),
                     $('<div class="col-xs-1"></div>').append(
                         $('<button id="fileList_delete" class="btn btn-lg sm-button edit-btn sm-delete">X</button>')
+                            .on('click',{ file: file }, deleteByName)
                     ),
                     $('<div class="col-xs-1"></div>')
                 )
             );
         }
     } resetFileList();
+
+    function deleteByName(event){
+        console.log(event.data.file.target);
+    }
 
 
 
