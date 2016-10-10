ad43109eebe34d6ad72587fc51449facee2790f4
Hugh <hughriegel@gmail.com>
2016-10-10 12:02:32 -0400

    some more refinement. Timeout now functions properly on theater

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use('/directors', directors);
 // will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res) {
-        res.status(err.status || 500);
+        res.status( 500);
         res.render('error', {
             message: err.message,
             error: err
--- public/javascripts/viewFiles.js
+++ public/javascripts/viewFiles.js
@@ -20,20 +20,28 @@
             $('#uploadedFileList').append(
                 $('<div class="row uploaded-file-row"></div>').attr('id',TARGET_PREFIX + file.target)
                     .append(
-                    $('<div class="col-xs-1"></div>'),
-                    $('<div class="col-xs-6"></div>').text(file.name)
-                        .attr('id', 'fileList_title' + i),
-                        $('<div id="fileList_extension" class="col-xs-1"></div>').text(file.extension),
-                        $('<div id="fileList_useType" class="col-xs-1"></div>').text(file.useType),
-                    $('<div class="col-xs-2"></div>').append(
-                        $('<button id="fileList_edit" class="btn btn-lg sm-button edit-btn sm-edit">Edit</button>')
-                            .on('click',{ file: file }, goToEditPage)
-                    ),
-                    $('<div class="col-xs-1"></div>').append(
-                        $('<button id="fileList_delete" class="btn btn-lg sm-button edit-btn sm-delete">X</button>')
-                            .on('click',{ file: file }, deleteByName)
-                    ),
-                    $('<div class="col-xs-1"></div>')
+                        $('<div class="col-xs-2"></div>')
+                            .append(
+                                $('<button id="fileList_play" class="btn btn-lg sm-button sm-play" data-toggle="modal" data-target="#playModal">Play</button>')
+                                    .on('click',{ file: file }, playFile)
+                            ),
+                        $('<div class="col-xs-4"></div>')
+                            .text(file.name)
+                            .attr('id', 'fileList_title' + i),
+                        $('<div id="fileList_extension" class="col-xs-1"></div>')
+                            .text(file.extension),
+                        $('<div id="fileList_useType" class="col-xs-1"></div>')
+                            .text(file.useType),
+                        $('<div class="col-xs-2"></div>')
+                            .append(
+                                $('<button id="fileList_edit" class="btn btn-lg sm-button sm-edit">Edit</button>')
+                                    .on('click',{ file: file }, goToEditPage)
+                        ),
+                        $('<div class="col-xs-2"></div>')
+                            .append(
+                                $('<button id="fileList_delete" class="btn btn-lg sm-button sm-delete">X</button>')
+                                    .on('click',{ file: file }, deleteByName)
+                    )
                 )
             );
         }
@@ -61,6 +69,11 @@
         window.location = "/editFile/" + file.target;
     }
 
+    function playFile(event){
+        var file = event.data.file;
+        $('#popupHeader').text(file.name);
+    }
+
     function removeRowByTarget(target){
         $('#' + TARGET_PREFIX + target).fadeOut(200, function(){
             $('#' + TARGET_PREFIX + target).remove();
@@ -70,4 +83,5 @@
     function fadeOutContainer(){
         $('.sm_fade-in').animate({ opacity: '0'}, 100);
     }
+
 })();
