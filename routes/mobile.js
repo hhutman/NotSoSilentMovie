cc854aa8b151eea0e46cb60d3767eca5d8677275
Hugh <hughriegel@gmail.com>
2016-11-12 14:40:20 -0500

    mobile page set up

--- routes/mobile.js
+++ routes/mobile.js
@@ -3,9 +3,23 @@ var express = require('express');
 var fs = require('fs');
 var router = express.Router();
 
+
+var database = require('../config/database');
+
+var Promise = require("bluebird");
+
+Promise.promisifyAll(database);
+Promise.promisifyAll(fs);
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('mobile');
+    database.getContent()
+        .then(function(files) {
+            res.render('mobile', {  files: files });
+        })
+        .catch(function(err) {
+            throw err;
+        });
 });
 
 
