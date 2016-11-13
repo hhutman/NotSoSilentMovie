c84dfecd584e8ab6ace73504734610ab7e50a5c5
Hugh <hughriegel@gmail.com>
2016-11-13 09:31:39 -0500

    queue div now concatenated correctly

--- public/javascripts/mobile_client.js
+++ public/javascripts/mobile_client.js
@@ -1,5 +1,10 @@
 function fileClick( target ){
-    var node = document.createElement("IMG");
-    node.setAttribute('src', "/thumbnails/" + target + ".png");
+    var node = document.createElement("DIV");
+    node.setAttribute('class',"col-xs-12");
+
+    var img_node = document.createElement("IMG");
+    img_node.setAttribute('src', "/thumbnails/" + target + ".png");
+    img_node.setAttribute('class',"sm-mobile-file_img");
+    node.appendChild(img_node);
     document.getElementById("queue").appendChild(node);
 }
