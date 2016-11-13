65d98a4ffbfa1edd1299a74d643ac1eb63d894dc
Hugh <hughriegel@gmail.com>
2016-11-13 09:07:12 -0500

    appending new images works to local queue

--- public/javascripts/mobile_client.js
+++ public/javascripts/mobile_client.js
@@ -1,5 +1,5 @@
-function fileClick( content ){
-    var node = document.createElement("div");
-    node.innerHTML = content.name;
+function fileClick( target ){
+    var node = document.createElement("IMG");
+    node.setAttribute('src', "/thumbnails/" + target + ".png");
     document.getElementById("queue").appendChild(node);
 }
