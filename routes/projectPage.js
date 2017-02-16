e0a5a7c3e7237fa252e8c53ef08bd3ea484248ed
Hugh <hughriegel@gmail.com>
2017-02-15 23:26:50 -0500

    a lot more server authentication - truncated -sanitizing - duplication - profanity

--- routes/projectPage.js
+++ routes/projectPage.js
@@ -1,15 +1,18 @@
 'use strict';
-var express = require('express');
-var fs = require('fs');
-var router = express.Router();
+const express = require('express');
+const fs = require('fs');
+const router = express.Router();
 
 
-var database = require('../config/database');
-var projectUpload = require('../controllers/sm-projectupload');
+const database = require('../config/database');
+const projectUpload = require('../controllers/sm-projectupload');
 
-var Promise = require("bluebird");
+const Promise = require("bluebird");
 
+const swearjar = require('swearjar');
 
+const minClips = 3;
+const textLimit = 10;
 
 Promise.promisifyAll(projectUpload);
 Promise.promisifyAll(database);
@@ -22,6 +25,9 @@ router.get('/', function(req, res) {
 router.post('/', function(req, res){
     const project = req.body;
 
+    project.creator = project.creator.replace(/[^0-9a-z]/gi, '');
+    project.name = project.name.replace(/[^0-9a-z]/gi, '');
+
     if(!project.name) {
         res.status(400).send("Invalid Movie Name");
         return;
@@ -30,8 +36,16 @@ router.post('/', function(req, res){
         res.status(400).send("Invalid Movie Creator");
         return;
     }
-    if(project.content.length < 3){
-        res.status(400).send("Invalid Movie Length - " + project.content.length + "/3");
+    if(project.content.length < minClips){
+        res.status(400).send("Invalid Movie Length - " + project.content.length + "/" + minClips);
+        return;
+    }
+    if(swearjar.profane(project.creator) || swearjar.profane(project.name)){
+        res.status(400).send("Profanity detected");
+        return;
+    }
+    if(project.creator.length > textLimit || project.name.length > textLimit){
+        res.status(400).send("Please shorten your labels to " + textLimit + " characters");
         return;
     }
 
@@ -42,7 +56,8 @@ router.post('/', function(req, res){
             res.end(JSON.stringify(response));
         })
         .catch(function(err){
-            res.status(400).send(err);
+            //TODO: Error can return all types, not just duplicate names
+            res.status(400).send("Name Already Exists");
         })
 });
 
