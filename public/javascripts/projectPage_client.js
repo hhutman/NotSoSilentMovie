5a29331c316ade4ae05f79604e676f0eb10bd6b4
Daxit Agarwal <dagarwa3@ncsu.edu>
2017-02-17 13:12:14 -0500

    Revert "Cleaned up merge conflicts"
    This reverts commit d6d95a7ab6c6d8bceb4c6b8f1471f51f36b1323d.

--- public/javascripts/projectPage_client.js
+++ public/javascripts/projectPage_client.js
@@ -21,6 +21,11 @@ window.onload = function () {
         group: "content",
         animation: 150,
     });
+<<<<<<< HEAD
+
+    //document.getElementById("sm-icon-makemovie").style.color = "#FFD447";
+=======
+>>>>>>> refs/remotes/origin/comingUp
 };
 
 function projectEdit (objectID){
