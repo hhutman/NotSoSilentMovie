074809fe562869ac3daf1f128923806927b7046a
Hugh <hughriegel@gmail.com>
2017-04-16 14:24:15 -0400

    Setting up for remote page. Added routes and views, but empty

--- routes/remoteLandingPage.js
+++ routes/remoteLandingPage.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('theater');
+    res.render('remoteLandingPage');
 });
 
 module.exports = router;
